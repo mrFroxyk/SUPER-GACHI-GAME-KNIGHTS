56cbbd70fdec897c44e07114d230dad51ef52132
mrFroxyk <anton.vedeniapin123@gmail.com>
2022-04-06 00:43:21 +0700

    Топ управление, кроме прыжков

--- kosmo/Assets/player.cs
+++ kosmo/Assets/player.cs
@@ -5,17 +5,50 @@ using UnityEngine;
 public class player : MonoBehaviour
 {
     CharacterController CharacterController;
-    public float speed = 1f;
-    public float rotate_speed = 3f;
-    // Start is called before the first frame update
+    public float speed = 2f;
+    public Transform cam;
+    private float rotationX = 0f;
+
+
     void Start()
     {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
         CharacterController = GetComponent<CharacterController>();
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0, Input.GetAxis("Mouse X")*rotate_speed, 0);
+        
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = 4f;
+        }
+        else
+        {
+            speed = 2f;
+        }
+        Quaternion qua = new Quaternion(0, cam.rotation.y, 0, cam.rotation.w);
+        transform.rotation = qua;
+        move();
+    }
+    void move()
+    {
+        rotationX += Input.GetAxis("Mouse X") * 3f;
+        transform.eulerAngles = new Vector3(0, rotationX, 0f);
+        Vector3 vector = new Vector3(Input.GetAxis("Horizontal")*2f, 0, Input.GetAxis("Vertical"));
+        vector = transform.TransformDirection(vector);
+        vector *= speed;
+        //CharacterController.SimpleMove(vector);
+        if (Input.GetButtonDown("Jump"))
+        {
+        }
+            
+        vector.y -= 200.0F * Time.deltaTime;
+        CharacterController.Move(vector * Time.deltaTime);
+
+        
     }
 }
--- kosmo/Assets/rotate_head.cs
+++ kosmo/Assets/rotate_head.cs
@@ -4,47 +4,28 @@ using UnityEngine;
 
 public class rotate_head : MonoBehaviour
 {
-    private float speed_rot;
+    private float speed_rot = 3f;
     public GameObject chid ;
     public player player;
-    public float rotationX = 0f;
-    public float rotationY = 0f;
+    private float rotationY = 0f;
+    private float rotationX = 0f;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        player = chid.GetComponent<player>();
-        speed_rot = player.rotate_speed;
-        Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Locked;
+
     }
     
 
     // Update is called once per frame
     void Update()
     {
-        float speed = 0.5f;
-        rot();
-        if (Input.GetKey(KeyCode.LeftShift))
-        {
-            speed = 1.5f;
-        }
-
-        transform.localPosition += new Vector3(transform.forward.x, 0, transform.forward.z) * Input.GetAxis("Vertical") * speed * 2.5f * Time.deltaTime;
-        transform.localPosition += new Vector3(transform.right.x, 0, transform.right.z) * Input.GetAxis("Horizontal") * speed * 2.5f * Time.deltaTime;
-        transform.Rotate(-Input.GetAxis("Mouse Y")*speed_rot,0,0);
-    }
-    void rot()
-    {
-        rotationX += Input.GetAxis("Mouse X") * 5f;
-        rotationY -= Input.GetAxis("Mouse Y") * 5f;
+        rotationX += Input.GetAxis("Mouse X") * speed_rot;
+        rotationY -= Input.GetAxis("Mouse Y") * speed_rot;
+        rotationY = Mathf.Clamp(rotationY, -90f, 75f);
         transform.eulerAngles = new Vector3(rotationY, rotationX, 0f);
-    }
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject.tag == "stena")
-        {
-            Debug.Log("pp");
-        }
+        
+
     }
 }
