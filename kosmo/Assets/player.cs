a8b76487c96bac3a6923843d12ccff337aa65991
mrFroxyk <anton.vedeniapin123@gmail.com>
2022-04-05 19:09:29 +0700

    новое управление чз характер

---
+++ kosmo/Assets/player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    CharacterController CharacterController;
+    public float speed = 1f;
+    public float rotate_speed = 3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CharacterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, Input.GetAxis("Mouse X")*rotate_speed, 0);
+    }
+}
---
+++ kosmo/Assets/rotate_head.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate_head : MonoBehaviour
+{
+    private float speed_rot;
+    public GameObject chid ;
+    public player player;
+    public float rotationX = 0f;
+    public float rotationY = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = chid.GetComponent<player>();
+        speed_rot = player.rotate_speed;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        float speed = 0.5f;
+        rot();
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = 1.5f;
+        }
+
+        transform.localPosition += new Vector3(transform.forward.x, 0, transform.forward.z) * Input.GetAxis("Vertical") * speed * 2.5f * Time.deltaTime;
+        transform.localPosition += new Vector3(transform.right.x, 0, transform.right.z) * Input.GetAxis("Horizontal") * speed * 2.5f * Time.deltaTime;
+        transform.Rotate(-Input.GetAxis("Mouse Y")*speed_rot,0,0);
+    }
+    void rot()
+    {
+        rotationX += Input.GetAxis("Mouse X") * 5f;
+        rotationY -= Input.GetAxis("Mouse Y") * 5f;
+        transform.eulerAngles = new Vector3(rotationY, rotationX, 0f);
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "stena")
+        {
+            Debug.Log("pp");
+        }
+    }
+}
