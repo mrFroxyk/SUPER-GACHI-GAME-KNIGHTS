2e0e6014475a51c9f7caea2d677df32d73c8a3c5
mrFroxyk <anton.vedeniapin123@gmail.com>
2022-05-18 23:50:24 +0700

    паралакс + пост.п

---
+++ My project (1)/Assets/script/paralax.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class paralax : MonoBehaviour
+{
+    [SerializeField] Transform followingTarget;
+    [SerializeField, Range(0f, 1f)] float parallaxStrength = 0.1f;
+    [SerializeField] bool disableVerticalParallax;
+    Vector3 targetPreviousPosition;
+    void Start()
+    {
+        if (!followingTarget)
+        {
+            followingTarget = Camera.main.transform;
+            
+        }
+        targetPreviousPosition = followingTarget.position;
+        Invoke("sc", 0.2f);
+    }
+    void Update()
+    {
+        Vector3 delta = followingTarget.position - targetPreviousPosition;
+        if (disableVerticalParallax)
+        {
+            delta.y = 0;
+        }
+        Debug.Log(delta.y);
+        
+        targetPreviousPosition = followingTarget.position;
+        transform.position += delta * parallaxStrength;
+    }
+    void sc()
+    {
+        disableVerticalParallax = false;
+    }
+}
+
+
+
+
