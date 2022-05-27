b762e9cf4eff69ca31f3a676d8f9f022eadbbae6
mrFroxyk <anton.vedeniapin123@gmail.com>
2022-05-27 19:48:20 +0700

    Сохры асасин, чет из юнити

---
+++ My project (1)/Assets/ss.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class ss : MonoBehaviour
+{
+    public Volume Volume;
+    bool Cern_Bel = false;
+    VolumeProfile profile;
+    void Start()
+    {
+        Volume = GetComponent<Volume>();
+        profile = Volume.sharedProfile;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            
+            //fog = profile.Add<>(false);
+           
+
+        }
+    }
+}
